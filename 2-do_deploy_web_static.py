529b959198ed322f65fb8df5a56e8d9edc25549a
Adefolaju Adegbuyi <rickfabulous201@yahoo.com>
2023-07-05 23:50:13 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["52.91.154.150", "35.174.205.58"]
 env.user = "ubuntu"
 
 
