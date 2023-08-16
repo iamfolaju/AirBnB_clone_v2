d1687b7d77dd9bff7f738ec62a37c25055c9e113
Adefolaju Adegbuyi <rickfabulous201@yahoo.com>
2023-08-16 17:20:18 +0100

    mod fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory
-and creates a unique FileStorage instance for your application"""
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from os import getenv
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    storage = DBStorage()
-else:
-    storage = FileStorage()
+storage = FileStorage()
 storage.reload()
